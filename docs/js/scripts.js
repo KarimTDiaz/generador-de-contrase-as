dfbcf4cefd69b5a8bbc6f29dac926f8c2fe40f22
Karim Tilouni Diaz <117732405+KarimTDiaz@users.noreply.github.com>
2023-01-11 18:57:39 +0100

    Generador de contraseñas a medias

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,29 +1,48 @@
 const rangeElement = document.getElementById('password-generator-range');
+const rangeElementText = document.getElementById('password-generator-length-number')
 const buttonGenerate = document.getElementById('button-generate')
 const formElement = document.getElementById('form')
 const finalPassword = document.getElementById('password-final-input')
-
-
+const checkBoxes = document.querySelectorAll('.check-input')
+let passwordLength = 0
+const alphabet = 'abcdefghijklmnñopqrstuvwxyz';
 
 formElement.addEventListener('submit', ev =>{
     ev.preventDefault()
+    finalPassword.value = createString(rangeElement.value)
+    checked()
 })
 
-
-rangeElement.addEventListener('change', (ev) =>{
-    ev.target.previousElementSibling.children[0].textContent = rangeElement.value
-
-});
-
-const createString = (length) =>{
-    const alphabet = 'abcdefghijklmnñopqrstuvwxyz'
-    let newString = '';
-    for (let i = 0; i < length; i++){
-        
+const checked = () =>{
+    for(const item of checkBoxes){
+        if(item.checked){
+            console.log(item.id + ' esta marcado')
+        }
     }
-    
-
-
 }
 
-createString(rangeElement.value)
+formElement.addEventListener('change', ev =>{
+    console.dir(ev.target.id)
+    passwordLength = rangeElement.value
+    rangeElementText.textContent = passwordLength
+})
+
+const createString = (length) =>{
+    let newString = '';
+    for (let i = 0; i < length; i++){
+        newString += alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+    }
+    return newString
+}
+
+const createStringUppercases = (length) =>{
+    const alphabetUpperCasse = alphabet.toUpperCase()
+    let newStringUpperCases = '';
+    for (let i = 0; i < length; i++){
+        newStringUpperCases += alphabetUpperCasse.charAt(Math.floor(Math.random() * alphabetUpperCasse.length))
+    }
+    return newStringUpperCases
+}
+
+    
+
