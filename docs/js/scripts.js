fc7f09887e5bc366098b0c7b7d157c58f605dabf
Karim Tilouni Diaz <117732405+KarimTDiaz@users.noreply.github.com>
2023-01-11 23:41:09 +0100

    Generador de contraseñas

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -5,7 +5,16 @@ const formElement = document.getElementById('form')
 const finalPassword = document.getElementById('password-final-input')
 const checkBoxes = document.querySelectorAll('.check-input')
 let passwordLength = 0
+let password = '';
+const upperElement = document.getElementById('uppercase')
+const lowerElement = document.getElementById('lowercase')
+const numberElement = document.getElementById('numbers')
+const symbolElement = document.getElementById('symbols')
 const alphabet = 'abcdefghijklmnñopqrstuvwxyz';
+const alphabetUpperCase = alphabet.toUpperCase();
+const numbers = '0123456789';
+const symbols = '!"·$%&/()^*{-ª';
+
 
 formElement.addEventListener('submit', ev =>{
     ev.preventDefault()
@@ -22,10 +31,22 @@ const checked = () =>{
 }
 
 formElement.addEventListener('change', ev =>{
-    console.dir(ev.target.id)
     passwordLength = rangeElement.value
     rangeElementText.textContent = passwordLength
+
+    const containsUpper = upperElement.checked;
+    const containsLower = lowerElement.checked;
+    const containsNumber = numberElement.checked;
+    const containsSymbol = symbolElement.checked;
+
+    console.log(containsUpper, containsLower, containsNumber, containsSymbol)
+    if(containsUpper){}
+
 })
+
+
+
+
 
 const createString = (length) =>{
     let newString = '';
@@ -35,14 +56,14 @@ const createString = (length) =>{
     return newString
 }
 
-const createStringUppercases = (length) =>{
+/* const createStringUppercases = (length) =>{
     const alphabetUpperCasse = alphabet.toUpperCase()
     let newStringUpperCases = '';
     for (let i = 0; i < length; i++){
         newStringUpperCases += alphabetUpperCasse.charAt(Math.floor(Math.random() * alphabetUpperCasse.length))
     }
     return newStringUpperCases
-}
+} */
 
     
 
