e9768cb3785cdf1b2ad30a3f0352b1ec0ef19edb
AlejandroJimenezMurciano <karimtilouni94@gmail.com>
2023-01-12 15:02:00 +0100

    Password generator

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -18,27 +18,44 @@ const symbols = '!"·$%&/()^*{-ª*^`¨´-.,=$';
 
 let passwordLength = 0
 let password = '';
-let checkedArray= [];
 
-
-/* const checked = () =>{
+const checked = () =>{
+    let newArray = [];
     for(const item of checkBoxes){
         if(item.checked){
-            checkedArray.push(item)
+            newArray.push(item)
         }
     }
-} */
+    return newArray
+}
 
+const strengthLevel = () => {
+    const checkedBoxes = checked()
 
-/* const strengthLevel = () => {
-    strengthLevelWord.textContent = '';
-    if(checkBoxes.length < 1){
-        strengthLevelWord.textContent='tooweak'
+    if(checkedBoxes.length === 0){
+        strengthLevelWord.textContent=''
     }
-    
-} */
+    if(checkedBoxes.length === 1){
+        strengthLevelWord.textContent='TOO WEAK'
+    }
+    if(checkedBoxes.length === 2){
+        strengthLevelWord.textContent='WEAK'
+    }
+    if(checkedBoxes.length === 3){
+        strengthLevelWord.textContent= 'MEDIUM'
+    }
+    if(checkedBoxes.length === 4){
+        strengthLevelWord.textContent= 'STRONG';
+    }
+    if(rangeElement.value < 5){
+        strengthLevelWord.textContent = 'TOO SHORT'
+        buttonGenerate.setAttribute('disabled' , true)
+    } 
 
-
+    if(checkBoxes.length === 0){
+        buttonGenerate.setAttribute('disabled' , true)
+    } 
+}
 
 const createString = (length) =>{
     let newString = '';
@@ -47,18 +64,13 @@ const createString = (length) =>{
     }
     return newString
 }
-
-formElement.addEventListener('change', ev =>{
-    passwordLength = rangeElement.value
-    rangeElementText.textContent = passwordLength
-
+const growString = () => {
     const containsUpper = upperElement.checked;
     const containsLower = lowerElement.checked;
     const containsNumber = numberElement.checked;
     const containsSymbol = symbolElement.checked;
 
     password = ''
-
     if (containsUpper){
         password +=  alphabetUpperCase
     }
@@ -71,13 +83,19 @@ formElement.addEventListener('change', ev =>{
     if(containsSymbol) {
         password +=  symbols
     }
-    
+}
+
+formElement.addEventListener('change', ev =>{
+    passwordLength = rangeElement.value
+    rangeElementText.textContent = passwordLength
+    checked()
+    strengthLevel()
+    growString()
 })
 
 formElement.addEventListener('submit', ev =>{
     ev.preventDefault()
     finalPassword.value = createString(rangeElement.value)
- /*    checked() */
 })
 
 
